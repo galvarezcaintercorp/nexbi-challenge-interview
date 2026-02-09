42fbc453546743e10e24ea9835ff97b653053556
Gaby Carbajal <galvarezca@intercorp.com.pe>
2026-02-09 16:30:31 -0500

    chore: no spoilers

--- src/app/store/pokemon-list.actions.ts
+++ src/app/store/pokemon-list.actions.ts
@@ -1,8 +1,3 @@
 /**
  * Your code here
  */
-
-export enum PokemonListActions {
-  SavePokemonList = '[Pokemon List] Save Pokemon list',
-  ClearPokemonList = '[Pokemon List] Clear Pokemon list',
-}
